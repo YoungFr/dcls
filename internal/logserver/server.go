8cf5776916c5294b063ef47f7bc8b971c8669b55
YoungFr <mengyl4772@gmail.com>
2024-02-02 22:42:58 +0800

    add more comments in gRPC server

--- internal/logserver/server.go
+++ internal/logserver/server.go
@@ -9,14 +9,20 @@ import (
 )
 
 // 这里的 CommitLog 是一个通用的日志存储结构需要实现的接口
-//
 // 这意味着我们在服务端真正使用的日志存储结构可以
 // 不使用 internal/log 目录下的实现的 Log 结构体
-//
-// 但是都必须实现 Append (追加一条记录) 和 Read (读取一条记录) 方法
+// 而是只要实现这三种方法即可
 type CommitLog interface {
+
+	// 将一条日志追加到日志存储结构中
+	// 成功时返回这条日志的下标
 	Append(*api.Record) (uint64, error)
+
+	// 给定一个下标读取对应的日志
+	// 成功时返回读取到的日志记录
 	Read(uint64) (*api.Record, error)
+
+	// 删除当前日志存储结构中的所有日志
 	Reset() error
 }
 
@@ -32,12 +38,19 @@ type LogImplConfig struct {
 
 // 根据实际使用的日志存储结构和服务器选项创建 gRPC 服务器
 func NewgRPCServer(c *LogImplConfig, opts ...grpc.ServerOption) (*grpc.Server, error) {
+	// 创建 gRPC 服务器的 3 个标准步骤
+	// 1
 	s := grpc.NewServer(opts...)
+
+	// 2
 	srv, err := newgRPCServer(c)
 	if err != nil {
 		return nil, err
 	}
+
+	// 3
 	api.RegisterLogServer(s, srv)
+
 	return s, nil
 }
 
@@ -77,8 +90,8 @@ func (s *gRPCServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api
 // 删除所有日志
 func (s *gRPCServer) Reset(ctx context.Context, req *api.ResetRequest) (*api.ResetResponse, error) {
 	if err := s.CommitLog.Reset(); err != nil {
-		return &api.ResetResponse{Ans: "Reset Failed"}, err
+		return &api.ResetResponse{Ans: "Reset FAILED"}, err
 	} else {
-		return &api.ResetResponse{Ans: "Reset OK"}, nil
+		return &api.ResetResponse{Ans: "Reset SUCCESS"}, nil
 	}
 }
