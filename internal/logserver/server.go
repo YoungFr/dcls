3e79d2372a6b6d647ced4567e51a09be4a4556b9
YoungFr <mengyl4772@gmail.com>
2024-02-10 00:05:57 +0800

    optimize log server implementation

--- internal/logserver/server.go
+++ internal/logserver/server.go
@@ -7,9 +7,11 @@ import (
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	api "github.com/youngfr/dcls/api/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-// 配置实际使用了哪一种日志存储结构实现及哪一种 ACL 访问控制实现
+// 配置实际使用了哪一种日志存储结构实现及哪一种访问控制实现
 type LogImplConfig struct {
 	CommitLog  CommitLog
 	Authorizer Authorizer
@@ -22,10 +24,6 @@ type gRPCServer struct {
 	api.UnimplementedLogServer
 }
 
-func newgRPCServer(c *LogImplConfig) (*gRPCServer, error) {
-	return &gRPCServer{LogImplConfig: c}, nil
-}
-
 // 根据实际使用的日志存储结构、访问控制机制和服务器选项创建 gRPC 服务器
 func NewgRPCServer(c *LogImplConfig, opts ...grpc.ServerOption) (*grpc.Server, error) {
 	opts = append(opts, grpc.UnaryInterceptor(
@@ -36,10 +34,7 @@ func NewgRPCServer(c *LogImplConfig, opts ...grpc.ServerOption) (*grpc.Server, e
 	s := grpc.NewServer(opts...)
 
 	// 2. 创建自己的服务器
-	srv, err := newgRPCServer(c)
-	if err != nil {
-		return nil, err
-	}
+	srv := &gRPCServer{LogImplConfig: c}
 
 	// 3. 调用 ProtoBuf 自动生成的注册方法
 	api.RegisterLogServer(s, srv)
@@ -47,7 +42,12 @@ func NewgRPCServer(c *LogImplConfig, opts ...grpc.ServerOption) (*grpc.Server, e
 	return s, nil
 }
 
+var errNoAuthorizationUsed = status.New(codes.Unauthenticated, "no authorization being used").Err()
+
 func (s *gRPCServer) Read(ctx context.Context, req *api.ReadRequest) (*api.ReadResponse, error) {
+	if s.Authorizer == nil {
+		return nil, errNoAuthorizationUsed
+	}
 	// 超级用户、普通用户和只读用户都可以读取日志
 	if err := s.Authorizer.Authorize(subject(ctx), objects, readAction); err != nil {
 		return nil, err
@@ -60,6 +60,9 @@ func (s *gRPCServer) Read(ctx context.Context, req *api.ReadRequest) (*api.ReadR
 }
 
 func (s *gRPCServer) Append(ctx context.Context, req *api.AppendRequest) (*api.AppendResponse, error) {
+	if s.Authorizer == nil {
+		return nil, errNoAuthorizationUsed
+	}
 	// 超级用户和普通用户可以追加日志
 	if err := s.Authorizer.Authorize(subject(ctx), objects, appendAction); err != nil {
 		return nil, err
@@ -77,6 +80,9 @@ const (
 )
 
 func (s *gRPCServer) Reset(ctx context.Context, req *api.ResetRequest) (*api.ResetResponse, error) {
+	if s.Authorizer == nil {
+		return nil, errNoAuthorizationUsed
+	}
 	// 只有超级用户可以删除所有日志
 	if err := s.Authorizer.Authorize(subject(ctx), objects, resetAction); err != nil {
 		return nil, err
